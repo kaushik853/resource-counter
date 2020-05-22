65b082d9b68d477a7d88580098bcbf2e8038d5ab
kaushik pal <kaushik853@gmail.com>
2020-05-22 17:38:31 +0200

    Added EKS and workspace counter

--- count_resources_mod.py
+++ count_resources_mod.py
@@ -3,7 +3,7 @@ import boto3
 import sys
 import botocore
 import json
-
+#pip install -U botocore==1.12.135 boto3==1.9.135
 # added and saved not printed yet
 mperm = {}
 
@@ -54,10 +54,10 @@ def controller(access, secret, profile, region,show_regions, savejson):
             sys.exit()
 
     # pull the account ID for use when needed for filtering
-    iam = session.client('sts')
+    sts_cli = session.client('sts')
 
     # account_id = iam.CurrentUser().arn.split(':')[4]
-    account_id = iam.get_caller_identity()["Account"]
+    account_id = sts_cli.get_caller_identity()["Account"]
     click.echo('Current account ID: ' + account_id)
 
     # Initialize dictionary to hold the counts. Pull the regions using EC2, since that is in every region.
@@ -190,6 +190,22 @@ def controller(access, secret, profile, region,show_regions, savejson):
         mperm[op] = {'Code':code,'Message':msg}
     try:
         rds_counter()
+    except botocore.exceptions.ClientError as e:
+        op = e.__dict__['operation_name']
+        code = e.__dict__['response']['Error']['Code']
+        msg = e.__dict__['response']['Error']['Message']
+        print('{0} {1} Operation: {2}'.format(code,msg,op))
+        mperm[op] = {'Code':code,'Message':msg}
+    try:
+        workspace_counter()
+    except botocore.exceptions.ClientError as e:
+        op = e.__dict__['operation_name']
+        code = e.__dict__['response']['Error']['Code']
+        msg = e.__dict__['response']['Error']['Message']
+        print('{0} {1} Operation: {2}'.format(code,msg,op))
+        mperm[op] = {'Code':code,'Message':msg}
+    try:
+     eks_counter()
     except botocore.exceptions.ClientError as e:
         op = e.__dict__['operation_name']
         code = e.__dict__['response']['Error']['Code']
@@ -714,6 +730,41 @@ def rds_counter():
         total_dbinstances += dbinstances_counter
         resource_counts[region]['rds instances'] = dbinstances_counter
     resource_totals['RDS Instances'] = total_dbinstances
+
+def workspace_counter():
+    region_list = session.get_available_regions(service_name='workspaces')
+
+    total_workspaces = 0
+
+    for region in region_list:
+        work_cli = session.client(service_name='workspaces', region_name=region)
+        workspace_counter = 0
+        workspace_paginator = work_cli.get_paginator('describe_workspaces')
+        workspace_iterator = workspace_paginator.paginate()
+        for workspace in workspace_iterator:
+            workspace_counter += len(workspace['Workspaces'])
+        total_workspaces += workspace_counter
+        resource_counts[region]['workspace'] = workspace_counter
+    resource_totals['Workspaces'] = total_workspaces
+
+def eks_counter():
+    region_list = session.get_available_regions(service_name='s3')
+    region_list.remove('us-west-1')
+    region_list.remove('us-west-2')
+
+    total_eks_clusters = 0
+
+    for region in region_list:
+        #resource_counts[region] = {}
+        eks_cli = session.client(service_name='eks', region_name=region)
+        eks_cluster_counter = 0
+        eks_paginator = eks_cli.get_paginator('list_clusters')
+        eks_iterator = eks_paginator.paginate()
+        for eks_cls in eks_iterator:
+            eks_cluster_counter += len(eks_cls['clusters'])
+        total_eks_clusters += eks_cluster_counter
+        resource_counts[region]['EKS'] = eks_cluster_counter
+    resource_totals['EKS'] = total_eks_clusters
 
 if __name__ == "__main__":
     controller()
--- test1.py
+++ test1.py
@@ -1,9 +1,57 @@
 import boto3
 from pprint import pprint
+import botocore
 
+global session
+global resource_counts
+global resource_totals
 
+resource_counts = {}
+resource_totals = {}
+mperm = {}
 session = boto3.session.Session(profile_name='adi_security')
-region_list = session.get_available_regions('accessanalyzer')
-acc_cli = session.client(service_name='accessanalyzer')
-for i in acc_cli.list_analyzers()['analyzers']:
-    print(i.get('arn'))
+#region_list = session.get_available_regions(service_name='s3')
+#print(region_list)
+#region_list1 = session.get_available_regions('rds')
+#print(region_list1)
+
+# region_list = session.get_available_regions('accessanalyzer')
+# acc_cli = session.client(service_name='accessanalyzer')
+# for i in acc_cli.list_analyzers()['analyzers']:
+#     print(i.get('arn'))
+
+# ec2_re = session.resource(service_name='ec2')
+# x = ec2_re.instances.all()
+# print(len(list(x)))
+
+def eks_counter():
+    region_list = session.get_available_regions(service_name='s3')
+    region_list.remove('us-west-1')
+    region_list.remove('us-west-2')
+
+    total_eks_clusters = 0
+
+    for region in region_list:
+        resource_counts[region] = {}
+        eks_cli = session.client(service_name='eks', region_name=region)
+        eks_cluster_counter = 0
+        eks_paginator = eks_cli.get_paginator('list_clusters')
+        eks_iterator = eks_paginator.paginate()
+        for eks_cls in eks_iterator:
+            eks_cluster_counter += len(eks_cls['clusters'])
+        total_eks_clusters += eks_cluster_counter
+        resource_counts[region]['EKS'] = eks_cluster_counter
+    resource_totals['EKS'] = total_eks_clusters
+
+#eks_counter()
+
+try:
+     eks_counter()
+except botocore.exceptions.ClientError as e:
+    op = e.__dict__['operation_name']
+    code = e.__dict__['response']['Error']['Code']
+    msg = e.__dict__['response']['Error']['Message']
+    print('{0} {1} Operation: {2}'.format(code,msg,op))
+    mperm[op] = {'Code':code,'Message':msg}
+print(resource_counts)
+print(resource_totals)
